1bedf15081a0acbae181836bcd4ee902f3c1ee6e
mx_lshk <max.loshak.i@gmail.com>
2023-12-24 00:34:08 +0200

    popular jobs : Welcome.tsx done : hooks folder

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -5,8 +5,12 @@ import { useRouter } from 'expo-router'
 import styles from './welcome.style'
 import { icons, SIZES, COLORS, FONTS } from '../../../constants'
 
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
+
+
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time");
   return (
     <View>
       <View style={styles.container}>
@@ -23,6 +27,33 @@ const Welcome = () => {
             placeholder="What are you looking for?"
           />
         </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => { }}>
+          <Image
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}>
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
       </View>
     </View>
   )
