5a9604e039a90ef3e5c0e823d85a089edb9d0583
mx_lshk <max.loshak.i@gmail.com>
2023-12-28 17:38:48 +0200

    job details page complete

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -25,16 +25,24 @@ const JobDetails = () => {
         switch (activeTab) {
             case "Qualifications":
                 return (
-                <Specifics 
-                    title='Qualifications'
-                    points={data[0].job_hilight?.qualifications ?? ['N/A']}
-                />
+                    <Specifics 
+                        title='Qualifications'
+                        points={data[0].job_highlights?.Qualifications ?? ['N/A']}
+                    />
                 )
-                break;
             case "About":
-                break;
+                return (
+                    <JobAbout 
+                        info={data[0].job_description ?? 'N/A'}
+                    />
+                )
             case "Responsibilities":
-                break;
+                return (
+                    <Specifics 
+                        title='Responsibilities'
+                        points={data[0].job_highlights?.Responsibilities ?? ['N/A']}
+                    />
+                )
             default:
                 break;
         }
@@ -105,6 +113,10 @@ const JobDetails = () => {
                         </View>
                     )}
                 </ScrollView>
+
+                <JobFooter 
+                    url={data[0]?.job_google_link?? 'https://careers.google.com/jobs/results/'}
+                />
             </>
         </SafeAreaView>
     )
